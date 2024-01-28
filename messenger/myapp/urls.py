2483f596cb461023c0dbf47bbdd566a2a7354c1c
LENOVO <LENOVO@DESKTOP-BH2L40U>
2024-01-28 16:35:57 +0200

    adjyst hrefs and add login/registration pages

--- messenger/myapp/urls.py
+++ messenger/myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "myapp"
 
 urlpatterns = [
-    path("",views.main_page,name="index")
+    path("",views.main_page,name="index"),
+    path("login",views.for_login,name="for_login"),
+    path("registration",views.for_registration,name="for_registration")
 ]

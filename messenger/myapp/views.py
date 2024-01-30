fcfe3d8884d9cc96d34e5cf6faa8e7f543dca384
LENOVO <LENOVO@DESKTOP-BH2L40U>
2024-01-30 16:56:20 +0200

    create 2 forms for login and registration

--- messenger/myapp/views.py
+++ messenger/myapp/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-# Create your views here.
+from django import forms
+
+class Login(forms.Form):
+    username = forms.CharField(widget=forms.EmailInput(attrs={"plasceholder":"Username"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Username"}))
+
+class Registration(forms.Form):
+    username = forms.CharField(widget=forms.EmailInput(attrs={"placeholder":"Username"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
+
 def main_page(request):
     return render(request,"myapp/main_page.html")
 
 def for_login(request):
-    return render(request,"myapp/logining.html")
+    return render(request,"myapp/logining.html",{
+        "login_form":Login(),
+    })
 
 def for_registration(request):
-    return render(request,"myapp/registration.html")
+    return render(request,"myapp/registration.html",{
+        "registration_form":Registration(),
+    })
+
